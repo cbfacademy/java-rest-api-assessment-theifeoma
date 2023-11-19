7eeda01990570ffc42fae5f3400ce52c2458d0ce
theifeoma <if.comokafor@ymail.com>
2023-11-19 13:13:15 +0000

    refactored endpoint format

--- src/main/java/com/cbfacademy/apiassessment/controllers/ClientTradeDetailsController.java
+++ src/main/java/com/cbfacademy/apiassessment/controllers/ClientTradeDetailsController.java
@@ -4,11 +4,9 @@ import com.cbfacademy.apiassessment.dto.ClientTradeDetails;
 import com.cbfacademy.apiassessment.services.ClientTradeDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -28,7 +26,7 @@ public class ClientTradeDetailsController {
     }
 
     // Endpoint for getting client trade details by product grouping and date range
-    @GetMapping("/by-date/{productGrouping}")
+    @GetMapping("/trades/by-date/{productGrouping}")
     public ResponseEntity<List<ClientTradeDetails>> getByProductAndDate(
             @PathVariable String productGrouping,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
@@ -38,7 +36,7 @@ public class ClientTradeDetailsController {
     }
 
     // Endpoint for getting client trade details by revenue range and product grouping
-    @GetMapping("/by-revenue/{productGrouping}")
+    @GetMapping("/trades/by-revenue/{productGrouping}")
     public ResponseEntity<List<ClientTradeDetails>> getByRevenueAndProduct(
             @RequestParam Long minRevenue,
             @RequestParam Long maxRevenue,
@@ -48,7 +46,7 @@ public class ClientTradeDetailsController {
     }
 
     // Endpoint for getting client trade details by client id and product grouping
-    @GetMapping("/by/{clientId}/{productGrouping}")
+    @GetMapping("/clients/{clientId}/trades/{productGrouping}")
     public ResponseEntity<List<ClientTradeDetails>> getByClientIdAndProduct(
             @PathVariable Long clientId,
             @PathVariable String productGrouping) {
@@ -57,7 +55,7 @@ public class ClientTradeDetailsController {
     }
 
     // Endpoint for getting client trade details by client id and date range
-    @GetMapping("/by-date-and-clientId")
+    @GetMapping("/trades/by-date-and-clientId")
     public ResponseEntity<List<ClientTradeDetails>> getByClientIdAndDate(
             @RequestParam Long clientId,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
@@ -65,31 +63,4 @@ public class ClientTradeDetailsController {
         List<ClientTradeDetails> result = clientTradeDetailsService.getByClientIdAndTimeRange(clientId, startDate, endDate);
         return ResponseEntity.ok(result);
     }
-
-//    @GetMapping("/by-product-grouping/{productGrouping}")
-//    public List<ClientTradeDetails> getByProductGrouping(@PathVariable String productGrouping) {
-//        return clientTradeDetailsService.getByProductGrouping(productGrouping);
-//    }
-//
-//    @GetMapping("/by-revenue-range")
-//    public List<ClientTradeDetails> getByRevenueRange(
-//            @RequestParam Long minRevenue,
-//            @RequestParam Long maxRevenue) {
-//        return clientTradeDetailsService.getByRevenueRange(minRevenue, maxRevenue);
-//    }
-//
-//    @GetMapping("/by-time-of-execution")
-//    public ResponseEntity<List<ClientTradeDetails>> getByTimeOfExecutionRange(
-//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
-//
-//        try {
-//            List<ClientTradeDetails> clients = clientTradeDetailsService.getByTimeOfExecutionRange(startDate, endDate);
-//            return new ResponseEntity<>(clients, HttpStatus.OK);
-//        } catch (NoSuchElementException e) {
-//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-//        } catch (RuntimeException e) {
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-//        }
-//    }
 }
